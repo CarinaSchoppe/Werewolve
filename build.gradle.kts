654e9c728535c025c9ece2b47c20652c341122d9
Carina Schoppe <info@carinaschoppe.com>
2026-01-01 13:22:55 +0100

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,25 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 group = "me.carinaschoppe"
 version = "1.0-SNAPSHOT"
 description = "A copy of the famous werewolf game with a chatgpt instance in it"
 plugins {
-    id("com.github.ben-manes.versions") version "0.53.0"
+    id("com.github.ben-manes.versions") version "0.53.0" apply false
 }
+
+
+subprojects {
+    apply(plugin = "com.github.ben-manes.versions")
+
+    tasks.withType<DependencyUpdatesTask>().configureEach {
+        rejectVersionIf {
+            val unstable = listOf("alpha", "beta", "rc", "m", "preview", "ea", "snapshot", "dev")
+            unstable.any { candidate.version.contains(it, ignoreCase = true) }
+        }
+    }
+}
+
+
 allprojects {
 
     repositories {
